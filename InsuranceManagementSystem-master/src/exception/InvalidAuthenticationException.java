97d5a9e7001e727fd8bba8be38fde06185b764e3
Suleyman <61500642+suleymanakay@users.noreply.github.com>
2022-06-28 19:42:01 +0300

    Add files via upload

---
+++ InsuranceManagementSystem-master/src/exception/InvalidAuthenticationException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class InvalidAuthenticationException extends Exception{
+
+    public InvalidAuthenticationException(String message) {
+        super(message);
+    }
+}
